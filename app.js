8e2005509fa485ad4ca09353ffdbdbadcfbc853c
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-02-13 22:00:13 +0100

    New startup application for test components

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import ReactDOM from 'react-dom';
+import BasicCollapse from './src/lib/Collapse/Collapse'
+
+ReactDOM.render(
+    <BasicCollapse title="Prueba"/>,
+    document.getElementById('collapse')
+);
