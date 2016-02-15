9e60b6a4bf0af0caad862783a60155f92c703f07
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-02-15 21:46:16 +0100

    Extract import react from .html to components

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 'use strict';
 
+import React from 'react';
 import ReactDOM from 'react-dom';
 import BasicCollapse from './src/lib/Collapse/Collapse'
 
 class DemoComponent extends React.Component {
+  toggleCollapse(){
+    console.log('Expand collapse');
+  }
   render() {
     return (
       <div>
         <h1>Components Demo</h1>
         <div id='collapse'>
-          <h2>Collapse Demo</h2>
+          <h2 ref='header'>Collapse Demo</h2>
           <BasicCollapse header="Prueba" element='section'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
