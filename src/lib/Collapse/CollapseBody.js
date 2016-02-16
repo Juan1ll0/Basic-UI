f7cb2486a0f01fbdbd0f305162de05b307da8692
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-02-16 22:08:54 +0100

    Extract styles to a css

--- src/lib/Collapse/CollapseBody.js
+++ src/lib/Collapse/CollapseBody.js
@@ -4,38 +4,26 @@ import React from 'react';
 import classNames from 'classnames';
 import {Motion, spring} from 'react-motion';
 
+// css
+import './css/CollapseBody.css';
+
 // import CollapseBodyContent from './CollapseBodyContent';
 
 export default class CollapseBody extends React.Component {
-  componentDidMount(){
-    // Get Height of childs.
-    // this.refs.content.clientHeight
-    console.log('Creando el cuerpo: ' + this.props.collapsed);
-    console.log('Creando el cuerpo: ' + this.refs.content.clientHeight);
-
-  }
   render() {
       if (this.props.collapsed) {
         return (
-          <div style={{height: 0, overflow: 'hidden'}}>
+          <div className='collapseBody collapsed'>
             <div ref='content'>{this.props.children}</div>
           </div>
         );
       } else {
-        // let contentHeight = this.refs.content.offsetHeight + ((this.refs.content.offsetHeight - this.refs.content.offsetTop)/2);
-        // let margin = (this.refs.content.offsetHeight - this.refs.content.offsetTop) / 2;
-        // let contentHeight = this.refs.content.offsetHeight + margin;
         let contentHeight = this.refs.content.clientHeight
         return (
           <Motion defaultStyle={{height: 0}} style={{height: spring(contentHeight)}}>
-          {({height}) => {
-              let componentStyle = {
-                border: '1px solid black',
-                height: height,
-                overflow: 'hidden'
-              }
-              return <div style={componentStyle} ref='content'>{this.props.children}</div>
-          }}
+            {({height}) => {
+                return <div style={{height: height}} className='collapseBody expanded' ref='content'>{this.props.children}</div>
+            }}
           </Motion>
         )
       }
