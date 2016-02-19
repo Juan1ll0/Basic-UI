5c51a83811dcedd733d42c27e64026b58e0da2d1
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-02-19 19:05:52 +0100

    Collapse: Change header classes with state

--- src/lib/Collapse/CollapseHeader.js
+++ src/lib/Collapse/CollapseHeader.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import classNames from 'classnames';
 
 import BasicCollapseHeaderIcon from './CollapseHeaderIcon';
 
@@ -8,13 +9,20 @@ import './css/CollapseHeader.css';
 
 export default class BasicCollapseHeader extends React.Component {
     render() {
+      let classes = classNames({
+                      'collapse-header': true,
+                      'collapse-header-expanded': !this.props.collapsed,
+                      'collapse-header-collapsed': this.props.collapsed
+                    });
+                    
       let icon = (this.props.icon)
                       ? <BasicCollapseHeaderIcon key='icon' {...this.props} />
                       : null;
 
       let title = <span key='title' className='collapse-header-title' onClick={this.props.toggleCollapse}>{this.props.title}</span>;
+
       return (
-        React.createElement(this.props.headerElement, {className: 'collapse-header'}, [icon, title])
+        React.createElement(this.props.headerElement, {className: classes}, [icon, title])
       );
     }
 }
@@ -23,11 +31,13 @@ BasicCollapseHeader.propTypes = {
   headerElement: React.PropTypes.node,
   title: React.PropTypes.string,
   icon: React.PropTypes.bool,
-  toggleCollapse: React.PropTypes.func.isRequired
+  toggleCollapse: React.PropTypes.func.isRequired,
+  collapsed: React.PropTypes.bool
 }
 
 BasicCollapseHeader.defaultProps = {
   headerElement: 'p',
   title: 'Collapse Header',
   icon: true,
+  collapsed: true
 }
