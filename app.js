3d25bed3f3a28c0b10b716a6af3c474854350cba
Juan Ignacio Fernández Díez <sr.jifernandez@gmail.com>
2016-02-19 19:02:47 +0100

    Demo Collapse wit button

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ import ReactDOM from 'react-dom';
 import BasicCollapse from './src/lib/Collapse/Collapse'
 
 class DemoComponent extends React.Component {
-  toggleCollapse(){
-    console.log('Expand collapse');
+  toggleDemonCollapse() {
+    this.refs.collapse2.toggleCollapsed();
   }
   render() {
     return (
       <div>
         <h1>Components Demo</h1>
         <div id='collapse'>
-          <h2 ref='header'>Collapse Demo</h2>
-          <BasicCollapse title="Prueba" element='section' icon={true}>
+          <h2>Collapse with header "h3"</h2>
+          <BasicCollapse title="Prueba" element='section' icon={true} headerElement='h3'>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -44,15 +44,19 @@ class DemoComponent extends React.Component {
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </BasicCollapse>
-          <BasicCollapse title="Prueba2" element='section'>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </BasicCollapse>
+          <h2>Collapse with button.</h2>
+          <div>
+            <button onClick={this.toggleDemonCollapse.bind(this)}>Open / Close</button>
+            <BasicCollapse element='section' ref='collapse2'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </BasicCollapse>
+          </div>
         </div>
       </div>
     )
